dd48d61e41b5d25422a6275a9be3d5152511420a
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-07-16 18:45:21 +0530

    feat: try datatypes in groovy

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,3 +1,14 @@
-static void main(String[] args) {
-  println "Hello world!"
+class Main {
+    static void main(String[] args) {
+        byte b = 10
+        short s = 20
+        int i = 3000
+        long l = 40000000
+        float f = 12.2
+        double d = 14.444444444
+        char c = 'a'
+        String str = "Mahendran is learning groovy"
+        boolean bool = true
+        printf("Byte is: %d\nShort is: %d\nInteger is: %d\nLong is: %d\nFloat is: %f\nDouble is: %f\nCharacter is: %c\nString is: %s\nBoolean is %b", b, s, i, l, f, d, c, str, bool)
+    }
 }
